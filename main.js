4ca49668f38602e3acdbaea9ad3cbf375d9fbcc9
vinncorp12 <160111689+vinncorp12@users.noreply.github.com>
2024-02-15 14:35:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
+jQuery(document).ready(function(){
+  jQuery('.nav-link').click(function(){
+    jQuery('.nav-link').removeClass("active");
+    jQuery(this).addClass("active");
+});
+});
