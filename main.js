3b1725fb5b773dd3cd24b7f902e93d4e19b715c6
vinncorp12 <osamaali@vinncorp.com>
2025-01-29 15:19:23 +0500

    changes on header and main pages

--- main.js
+++ main.js
@@ -4,39 +4,85 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
-jQuery(document).ready(function(){
-  jQuery('.nav-link').click(function(){
-    jQuery('.nav-link').removeClass("active");
+jQuery(document).ready(function () {
+  jQuery(".nav-link").click(function () {
+    jQuery(".nav-link").removeClass("active");
     jQuery(this).addClass("active");
-});
+  });
 });
 
-jQuery(document).ready(function() {
-	jQuery(".hamburger").click(function() {
-    if(jQuery(".hamburger").has(".active")){
-      jQuery('html').toggleClass('html-burger-open');
-    jQuery('body').toggleClass('html-burger-open');
+jQuery(document).ready(function () {
+  jQuery(".hamburger").click(function () {
+    if (jQuery(".hamburger").has(".active")) {
+      jQuery("html").toggleClass("html-burger-open");
+      jQuery("body").toggleClass("html-burger-open");
     }
+  });
 });
+
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector("#password");
+
+// togglePassword.addEventListener("click", function (e) {
+
+//   const type =
+//     password.getAttribute("type") === "password" ? "text" : "password";
+//   password.setAttribute("type", type);
+
+//   this.classList.toggle("fa-eye-slash");
+// });
+
+jQuery(window).scroll(function () {
+  var scroll = jQuery(window).scrollTop();
+  if (scroll >= 100) {
+    jQuery("header").addClass("fixed");
+  } else {
+    jQuery("header").removeClass("fixed");
+  }
 });
 
+jQuery(document).ready(function () {
+  "use strict";
 
+  //Scroll back to top
 
-
-const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#password');
-
-  togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 50;
+  var duration = 550;
+  jQuery(window).on("scroll", function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".progress-wrap").addClass("active-progress");
+    } else {
+      jQuery(".progress-wrap").removeClass("active-progress");
+    }
+  });
+  jQuery(".progress-wrap").on("click", function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, duration);
+    return false;
+  });
 });
